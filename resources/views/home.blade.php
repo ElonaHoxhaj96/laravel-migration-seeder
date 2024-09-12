81006b7f1533f3be7f4f9c8c707a2e6310ac7caa
ElonaHoxhaj96 <167321131+ElonaHoxhaj96@users.noreply.github.com>
2024-09-12 16:21:48 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+{{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
+@extends('layouts.main')
+
+
+@section('content')
+<div class="container my-5">
+    <h1>Home</h1>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita facilis porro beatae nobis, quia quis animi voluptatum? Odio expedita ad eius odit laudantium, aliquid, ullam blanditiis amet, beatae veniam ab!</p>
+</div>
+
+@endsection
+
+
+@section('titlePage')
+    home
+@endsection
